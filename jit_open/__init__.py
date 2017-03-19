29744994707f5f3cf73d31c0555035b8279e8a3d
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2017-03-19 16:50:21 +0100

    Fixed import.

--- jit_open/__init__.py
+++ jit_open/__init__.py
@@ -7,7 +7,7 @@ Copyright (c) 2017 Jeroen F.J. Laros <J.F.J.Laros@lumc.nl>
 
 Licensed under the MIT license, see the LICENSE file.
 """
-from .jit_open import JITOpen
+from .jit_open import Handle, Queue
 
 
 __version_info__ = ('0', '0', '2')
