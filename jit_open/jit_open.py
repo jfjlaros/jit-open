d263157e1d897a5d15b3ade77e524635fb1b396a
J.F.J. Laros <J.F.J.Laros@lumc.nl>
2018-09-07 13:58:09 +0200

    Added append-open class.

--- jit_open/jit_open.py
+++ jit_open/jit_open.py
@@ -9,3 +9,14 @@ class JITOpen(object):
         self._handle = open(*self._args, **self._kwargs)
         self.write = self._handle.write
         self.write(*args, **kwargs)
+
+
+class AppendOpen(object):
+    def __init__(self, name, **kwargs):
+        self._name = name
+        self.write = self._append
+
+    def _append(self, *args, **kwargs):
+        handle = open(self._name, 'a+')
+        handle.write(*args, **kwargs)
+        handle.close()
